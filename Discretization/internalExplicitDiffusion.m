77f7c85e3baad5c80c62fde3ae5b28aa0c477872
LioSergent <lio.sergent@gmail.com>
2023-07-17 11:32:23 +0200

    remove useless discretization functions

--- Discretization/internalExplicitDiffusion.m
+++
@@ -1,9 +0,0 @@
-function RHS = internalExplicitDiffusion(D, var_motrice)
-    m = var_motrice.domain;
-    grads = gradientTerm(var_motrice);
-    dz = m.cellsize.x(2:end-1);
-    Jw = [0; D.xvalue(2:end-1) .* grads.xvalue(2:end-1)];
-    Je = [D.xvalue(2:end-1) .* grads.xvalue(2:end-1); 0];
-    RHS = (Jw - Je)./ dz;
-    RHS = [0; RHS; 0];
-end
